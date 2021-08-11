971183e812141b043620712f3cf84f1912ade8ed
pipimiao <hsfzmm@163.com>
2021-08-11 18:34:08 +0800

    配置装饰器写法

--- master-react/config-overrides.js
+++ master-react/config-overrides.js
@@ -5,10 +5,18 @@
  */
 
 // 从customize-cra引入一些相关的方法
-const {override,addLessLoader} = require('customize-cra');
+const {override,addLessLoader,fixBabelImports,addDecoratorsLegacy} = require('customize-cra');
+const modifyVars = require('./lessVars');
 
 module.exports = override(
   addLessLoader({
-    javascriptEnabled:true
-  })
+    javascriptEnabled:true,
+    modifyVars
+  }),
+  fixBabelImports('import',{
+    libraryName:'antd',
+    libraryDirectory:'es',
+    style:true
+  }),
+  addDecoratorsLegacy()
 )
--- master-react/src/App.js
+++ master-react/src/App.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react'
+import {Button} from 'antd';
 
-export default class App extends Component {
+const testHoc = (WrappedComponent) => {
+  return class HOCComponent extends Component{
+    render(){
+      return(
+        <>
+          <WrappedComponent/>
+          <div>高阶组件信息</div>
+        </>
+      )
+    }
+  }
+}
+
+@testHoc
+ class App extends Component {
   render() {
     return (
       <div>
         app
+        <Button type="primary">测试</Button>
       </div>
     )
   }
 }
+
+export default App;
+
